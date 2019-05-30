fed7659eba7f7c08e1a629373e81f14a4a8bfcdc
AO Industries, Inc. <support@aoindustries.com>
2019-05-30 10:38:51 -0500

    Updated project description to "High performance streaming character encoding."
    Adding "streaming" better describes the purpose of this library.

--- src/main/java/com/aoindustries/encoding/UrlInXhtmlEncoder.java
+++ src/main/java/com/aoindustries/encoding/UrlInXhtmlEncoder.java
@@ -1,6 +1,6 @@
 /*
- * ao-encoding - High performance character encoding.
- * Copyright (C) 2009, 2010, 2011, 2013, 2015, 2016  AO Industries, Inc.
+ * ao-encoding - High performance streaming character encoding.
+ * Copyright (C) 2009, 2010, 2011, 2013, 2015, 2016, 2019  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
