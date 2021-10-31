3e6aac8caef5e03263d102cce2c4eb0badc9df1a
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:54:33 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- devel/src/main/java/com/aoapps/encoding/i18n/ApplicationResources.java
+++ devel/src/main/java/com/aoapps/encoding/i18n/ApplicationResources.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-encoding.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ao-encoding.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.aoapps.encoding.i18n;
 
