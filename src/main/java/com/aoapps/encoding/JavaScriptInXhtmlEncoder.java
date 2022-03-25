e12e94a12c99b899d1a7837f7f19a5d0e09a62ec
AO Industries, Inc <support@aoindustries.com>
2022-03-25 16:16:29 -0500

    < > & are not encoded due to CDATA block

--- src/main/java/com/aoapps/encoding/JavaScriptInXhtmlEncoder.java
+++ src/main/java/com/aoapps/encoding/JavaScriptInXhtmlEncoder.java
@@ -1,6 +1,6 @@
 /*
  * ao-encoding - High performance streaming character encoding.
- * Copyright (C) 2009, 2010, 2011, 2012, 2013, 2015, 2016, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2009, 2010, 2011, 2012, 2013, 2015, 2016, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -49,8 +49,7 @@ public final class JavaScriptInXhtmlEncoder extends MediaEncoder {
 	/**
 	 * Encodes a single character and returns its String representation
 	 * or null if no modification is necessary.  Any character that is
-	 * not valid in XHTML, or is '&lt;', '&amp;', or '&gt;' is encoded to
-	 * JavaScript \\uxxxx escapes.
+	 * not valid in XHTML is encoded to JavaScript \\uxxxx escapes.
 	 */
 	private static String getEscapedCharacter(char ch) {
 		// These characters are allowed in JavaScript but need encoded for XHTML
