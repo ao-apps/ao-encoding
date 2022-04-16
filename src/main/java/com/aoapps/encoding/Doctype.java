7cce729c83c01f4705e76bc0f5bcd617b505ad54
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:34:49 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/encoding/Doctype.java
+++ src/main/java/com/aoapps/encoding/Doctype.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-encoding.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.encoding;
 
 import static com.aoapps.encoding.TextInXhtmlAttributeEncoder.encodeTextInXhtmlAttribute;
