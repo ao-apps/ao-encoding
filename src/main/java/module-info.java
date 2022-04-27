233857f7688645f223c507426f87749c05215a46
AO Industries, Inc <support@aoindustries.com>
2022-04-26 23:57:40 -0500

    Activated org.openrewrite.java.cleanup.Cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -29,4 +29,4 @@ module com.aoapps.encoding {
   requires static jsr305; // <groupId>com.google.code.findbugs</groupId><artifactId>jsr305</artifactId>
   // Java SE
   requires java.logging;
-}
+} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation
